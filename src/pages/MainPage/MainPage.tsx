d3045d1f76d0f9c8142178285bcc6c4297f3bbd1
Dyniaaa <piotrkkardynal@gmail.com>
2025-10-12 23:51:17 +0200

    Add about section and styles, fix navigation margin

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "../../sections/MainPage/Hero/Hero";
 import Benefits from "../../sections/MainPage/Benefits/Benefits";
+import About from "../../sections/MainPage/About/About";
 
 const Main: React.FC = () => {
   return (
     <>
       <Hero />
       <Benefits />
+      <About />
     </>
   );
 };
